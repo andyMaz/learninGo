9fc5e24e1aa202e366e717f014092278788896cd
andyMaz <suegreene256@yahoo.com>
2016-06-16 17:47:12 +0100

    sample git

--- 02_package/main/main.go
+++ 02_package/main/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
-	//"fmt"
 	"github.com/andyMaz/learninGo/02_package/stringutil"
 	"fmt"
 )
+var s string
 
+var f float64
 func main() {
+	q := "Danny"
+	s = "Joey"
+	i := 1
+	fmt.Println(i)
+	fmt.Println(s)
+	fmt.Println(f)
+	fmt.Println(q)
 	stringutil.Name()
 	fmt.Println(stringutil.Reverse("Andy"))
 }
