e37405d0895d5cfdf1c0673eefb61cc01d44ede9
andyMaz <suegreene256@yahoo.com>
2016-06-20 03:47:09 +0100

    sample git

--- 03_package/main/main.go
+++ 03_package/main/main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	fmt.Println("03_package")
 	Loop1(10)
-	printNums(7)
+	printNums(10)
 	ary()
 }
 
@@ -31,6 +31,10 @@ func printNums(n int) {
 		case 3: fmt.Println("Three")
 		case 4: fmt.Println("Four")
 		case 5: fmt.Println("Five")
+		case 6: fmt.Println("Six")
+		case 7: fmt.Println("Seven")
+		case 8: fmt.Println("Eight")
+		case 9: fmt.Println("Nine")
 		default: {
 			fmt.Println("Unknown Number")
 			break Loop
