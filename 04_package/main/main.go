c0fa648188194e015262a6823c8d815dead049cb
Andrei Rika <andrei.rika@gmail.com>
2016-06-22 01:47:24 +0100

    sample git

--- 04_package/main/main.go
+++ 04_package/main/main.go
@@ -1,14 +1,36 @@
 package main
- import (
-	 "net/http"
-	 "io/ioutil"
-	 "fmt"
- )
+
+import "fmt"
+
+const (
+	pi =  3.14
+	Language = "Go"
+)
+
+const (
+	A = iota
+	B = iota
+	C = iota
+	D = iota
+)
+
+const (
+	_ = iota
+	X = 1 << (iota * 10)
+	Y = 1 << (iota * 10)
+)
 
 func main() {
-	res, _ := http.Get("http://www.bbc.co.uk/")
-	page, _ := 	 ioutil.ReadAll(res.Body)
-	res.Body.Close()
-	fmt.Printf("%s", page)
+	fmt.Printf("%3.2f\n", 3.14)
+	fmt.Printf("%s\n", Language)
+
+	fmt.Println(A)
+	fmt.Println(B)
+	fmt.Println(C)
+	fmt.Println(D)
+	fmt.Println()
+	fmt.Println(X)
+	fmt.Println(Y)
+
 
 }
