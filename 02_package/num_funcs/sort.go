c864400647949142a17f1be53e035e24a47418c5
Andrei Rika <andrei.rika@gmail.com>
2016-06-29 19:48:00 +0100

    sample git

--- 02_package/num_funcs/sort.go
+++ 02_package/num_funcs/sort.go
@@ -1,18 +1,20 @@
 package num_funcs
 
 func BubbleSort(a []int) {
-	var temp int
 	var n = len(a)
 	for i := 0; i < n; i++  {
 		for j := 1; j < n-i; j++ {
 			if a[j-1] > a[j]  {
-				temp =  a[j-1]
-				a[j-1] = a[j]
-				a[j] = temp
+				a[j-1], a[j] = swap(a[j-1], a[j])
 			}
 		}
 	}
 }
+
+func swap(x, y int) (int, int) {
+	return y, x
+}
+
 
 func InsertSort(a []int) {
 	for i := 1; i < len(a); i++ {
