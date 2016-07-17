ee2c81d1658668b65a8344ae65e5c99d12bb6fed
Andrei Rika <andrei.rika@gmail.com>
2016-07-17 19:20:56 +0100

    calculating prime numbers

---
+++ prime/main/prime.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+	"math"
+)
+
+var initials = [11]int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31}
+var primes = list.New()
+
+func main() {
+	for _, v := range initials {
+		primes.PushBack(v)
+	}
+	primeNumbers(100)
+	count := 0
+	for e := primes.Front(); e != nil; e = e.Next() {
+		if count%10 == 0 {
+			fmt.Println()
+		}
+		fmt.Printf("%d\t", e.Value)
+		count++
+	}
+	fmt.Println()
+}
+
+func primeNumbers(num int) {
+	for n := 33; n <= num; n += 2 {
+		if isPrime(n) {
+			primes.PushBack(n)
+		}
+	}
+}
+
+func isPrime(num int) bool {
+	for n := primes.Front(); n != nil; n = n.Next() {
+		sqrt := math.Sqrt(float64(num))
+		if n.Value.(int) <= int(sqrt) {
+			if num%n.Value.(int) == 0 {
+				return false
+			}
+		} else {
+			return true
+		}
+	}
+	return false
+}
